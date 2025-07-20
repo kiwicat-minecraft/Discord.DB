12dcfa6c7cb14a4096a69923ed96b5c81e5e2a1a
kiwicat-minecraft <emgekilian@outlook.de>
2025-07-20 14:48:34 +0200

    Upload: player.gd by kiwicato_o on Discord

---
+++ uploads/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+
+
+const SPEED = 3.0
+const JUMP_VELOCITY = 4.5
+
+
+
+func _process(_delta: float) -> void:
+	$CanvasLayer/Label.text = "Needed Item: " + Global.needed_scan
+	$CanvasLayer/Label2.text ="Delivered: " + str(Global.boxes)
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("Jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if Input.is_action_just_pressed("Exit"):
+		get_tree().quit()
+
+	move_and_slide()
